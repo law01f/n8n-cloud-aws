b57e243855e24684d0a6a5c0a7b1180ef1fe9df2
Blakkos <129810804+Blakkos@users.noreply.github.com>
2025-02-19 15:43:39 -0500

    Update n8n_cloud_update.sh

--- n8n_cloud_update.sh
+++ n8n_cloud_update.sh
@@ -2,7 +2,7 @@
 my_current_date=$(date +"%d%b%Y_%H%M%S%Z")
 mkdir ~/n8n_bk_$my_current_date
 read -p "Enter the name of your n8n container (You can use sudo docker ps to find out.): " My_n8n_container_name
-read -p "Enter the name of the docker volume mapped to the /home/node/.n8n floder located inside the container (/!\ IMPORTANT /!\ If you do not have that, please cancel this process with Ctrl+C): " My_n8n_volume
+read -p "Enter the name of the docker volume mapped to the /home/node/.n8n folder located inside the container (/!\ IMPORTANT /!\ If you do not have that, please cancel this process with Ctrl+C): " My_n8n_volume
 read -p "Enter your domain or subdomain name (Example:my-n8n.ddns.net): " My_Domain_Or_Subdomain
 echo -e "********************************************************************\n********************************************************************\n*******************n8n backup process started...********************\n********************************************************************\n********************************************************************\n"
 echo "Backing up all workflows..."
@@ -16,6 +16,9 @@ echo "All credentials are backed up!"
 echo "Backing up config file..."
 sudo docker cp n8n:/home/node/.n8n/config ~/n8n_bk_$my_current_date/config_n8n_bk
 echo "Config file is backed up!"
+echo "Backing up /home/node/.n8n folder..."
+sudo docker cp n8n:/home/node/.n8n/ ~/n8n_bk_$my_current_date/.n8n_bk
+echo "/home/node/.n8n folder is backed up!"
 echo -e "********************************************************************\n********************************************************************\n****************** n8n backup process ended! ***********************\n********************************************************************\n********************************************************************\n"
 echo -e "********************************************************************\n********************************************************************\n****************** n8n update process started... *******************\n********************************************************************\n********************************************************************\n"
 sudo docker stop $My_n8n_container_name
@@ -27,5 +30,4 @@ sudo docker pull n8nio/n8n:latest
 echo "The latest n8n image has been pulled!"
 echo "Now starting updated n8n container..."
 sudo docker run -d --restart unless-stopped -it --name n8n -p 5678:5678 -e N8N_HOST="$My_Domain_Or_Subdomain" -e WEBHOOK_TUNNEL_URL="https://$My_Domain_Or_Subdomain/" -e WEBHOOK_URL="https://$My_Domain_Or_Subdomain/" -v ~/.n8n:/root/.n8n -v ~/n8n-local-files:/files -v $My_n8n_volume:/home/node/.n8n n8nio/n8n:latest
-echo -e "********************************************************************\n********************************************************************\n****************** n8n update process ended!... ********************
-\n********************************************************************\n********************************************************************\n"
+echo -e "********************************************************************\n********************************************************************\n****************** n8n update process ended!... ********************\n********************************************************************\n********************************************************************\n"
